36ce21c0c443185980a28e4666fe355856539aba
FELIPE SILVA DOS SANTOS FONTES <46290704869@info.local>
2025-12-02 17:54:11 -0300

    Finalizando as pendentes

--- Banco de Dados/Scripts/SQL Devconnect/SQL 6.sql
+++ Banco de Dados/Scripts/SQL Devconnect/SQL 6.sql
@@ -1,12 +1,12 @@
 --DML
 
--- Inserindo usuários
+-- Inserindo usuarios
 INSERT INTO Usuario (UsuarioId, NomeCompleto, NomeUsuario, Email, Senha, FotoPerfilUrl)
 VALUES
-(1, 'João da Silva', 'joaos', 'joao@email.com', 'senha123', 'url1.jpg'),
-(2, 'Maria Oliveira', 'mariao', 'maria@email.com', 'senha456', 'url2.jpg');
+(1, 'JoÃ£o da Silva', 'joaos', 'joao@email.com', 'senha123', 'url1.jpg'),
+(2, 'Maria Oliveira', 'maria', 'maria@email.com', 'senha456', 'url2.jpg');
 
--- Inserindo publicações
+-- Inserindo publicaÃ§Ãµes
 INSERT INTO Publicacao (PublicacaoId, UsuarioId, Descricao, ImagemUrl, DataPublicacao)
 VALUES
 (101, 1, 'Minha primeira foto!', 'foto1.jpg', '2025-10-01 14:00:00'),
@@ -18,11 +18,11 @@ VALUES
 (1, 2, 101),
 (2, 1, 102);
 
--- Inserindo comentários
+-- Inserindo comentarios
 INSERT INTO Comentario (ComentarioId, UsuarioId, PublicacaoId, Texto, DataComentario)
 VALUES
 (1, 2, 101, 'Muito legal!', '2025-10-01 15:00:00'),
-(2, 1, 102, 'Bom dia pra você também!', '2025-10-01 10:00:00');
+(2, 1, 102, 'Bom dia pra voce tambem!', '2025-10-01 10:00:00');
 
 -- Inserindo seguidores
 INSERT INTO Seguidor (UsuarioId, SeguidorId, PublicacaoId)
